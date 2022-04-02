4fb99f384f7dc076d500f98479fabfca6523ed1a
Andrew Morton <andrewmorton2@hotmail.co.uk>
2022-04-02 18:31:01 +0100

    Rectangle intersection working

--- states/overworld.py
+++ states/overworld.py
@@ -8,9 +8,18 @@ class Overworld(BaseState):
         self.title_rect = self.title.get_rect(center=self.screen_rect.center)
         self.next_state = "MEAL DEAL MANIA"
         self.time_active = 0
-
+        # Character
         self.character_rect = pygame.Rect((0,0), (50,50))
         self.character_rect.center = self.screen_rect.center
+        # Location icons
+        self.location_rects = {}
+        self.initialise_locations()
+    
+    def initialise_locations(self):
+        print(self.screen_rect.center)
+        self.location_rects["MEAL DEAL MANIA"] = pygame.Rect((0,0), (50,50))
+        self.location_rects["MEAL DEAL MANIA"].center = (740, 360)
+
 
     # Runs continuously, it's the loop
     def update(self, dt):
@@ -19,7 +28,13 @@ class Overworld(BaseState):
         # if self.time_active >= 5000:
         #     self.done = True
 
+        # Move character
         self.handle_keys(self.character_rect)
+        # Maybe use pygame.Rect.collidedict instead...
+        if self.character_rect.collidelist(list(self.location_rects.values())) == 0:
+            print("intersecc")
+
+        
         pass
 
     # Handles arrow keys for moving character
@@ -38,16 +53,12 @@ class Overworld(BaseState):
     def get_event(self, event):
         if event.type == pygame.QUIT:
             self.quit = True
-        elif event.type == pygame.KEYDOWN:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_DOWN]:
-                self.character_rect.move_ip(0, -10)
-            
-        # elif event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_UP:
-        #         self.character_rect.move_ip(0, -10)
 
     def draw(self, surface):
         surface.fill(pygame.Color("green"))
         surface.blit(self.title, self.title_rect)
+        # Draw character rect
         pygame.draw.rect(surface, pygame.Color("blue"), self.character_rect)
+        # Draw locations rects
+        for loc_rect in self.location_rects.values():
+            pygame.draw.rect(surface, pygame.Color("red"), loc_rect)
