d43a1081543e1ee43127023300dda4381abc2139
Andrew Morton <andrewmorton2@hotmail.co.uk>
2022-04-03 04:29:37 +0100

    kinda meal deal mania and main menu

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import sys
 import pygame
 from states.meal_deal_mania import MealDealMania
 from states.overworld import Overworld
+from states.main_menu import MainMenu
 # minigame states here
 from game import Game
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 states = {
+    "MAIN MENU": MainMenu(),
     "OVERWORLD": Overworld(),
     # minigame states here
     "MEAL DEAL MANIA": MealDealMania()
 }
 
-game = Game(screen, states, "OVERWORLD")
+game = Game(screen, states, "MAIN MENU")
 game.run()
